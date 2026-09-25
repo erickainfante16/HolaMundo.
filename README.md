bda0e08a14da3d0c25c72cde349199ed4b625b9c
erickainfante16 <ericka.infante16@gmail.com>
2026-09-25 11:39:10 -0600

    Proyecto iniciado

---
+++ README.md
@@ -0,0 +1,3 @@
+Este es mi primer "HolaMundo"
+#Se puede probar mediante los siguientes comandos:
+py main.py
---
+++ Variables/variables.py
@@ -0,0 +1,59 @@
+#Variable: espacio en memoria que se utiliza para almacenar cierto tipo de dato.
+
+#Tipos de variables
+
+#Guardamos texto dentro de una variable llamada nombre
+nombre = "Ricardo"
+print (nombre)
+print (type(nombre))
+
+name_1 = "Daniel"
+edad = 25
+ciudad = "México"
+
+print(name_1)
+print(edad)
+print (ciudad)
+
+print(type(name_1))
+print(type(edad))
+print(type(ciudad))
+
+# Enteros (int)
+num_1 = 5
+num_2 = 10
+print(num_1)
+print(num_2)
+
+# Operadores
+print(num_1 + num_2)#Output 15
+print(num_1 - num_2)#Output -5
+print(num_1 * num_2)#Output 50
+print(num_1 / num_2)#Output 0.5 División con decimal
+print(num_1 // num_2)#Output 0 División entera
+"""
+# Flotantes (float)
+precio_1 = 99.99
+precio_2 = 5.5
+calif = 8.9
+
+print(precio_1 + precio_2)
+print(type(precio_1))
+"""
+# Booleanos (verdadero o falso, 1 o 0)
+activo = True #Verdadero = 1
+finalizado = False #Falso = 0
+
+print(activo)
+print(finalizado)
+
+print(type(activo))
+print(type(finalizado))
+
+num1 = int("5")
+num2 = int("10")
+print(num1)
+print(num2)
+
+print(type(num1))
+print(type(num2))
